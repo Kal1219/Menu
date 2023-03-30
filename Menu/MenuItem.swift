f9e57c21fc99ca57a95b411e3b84fa535f369dfe
Kal1219 <kaleb.cortes1208@gmail.com>
2023-03-30 15:26:00 -0600

    Creación de leccionarios 1 a 4 del menú en swiftUI y el arreglo de variables para el menú

---
+++ Menu/MenuItem.swift
@@ -0,0 +1,15 @@
+//
+//  MenuItem.swift
+//  Menu
+//
+//  Created by Kaleb on 30/03/23.
+//
+
+import Foundation
+
+struct MenuItem
+{
+    var name:String
+    var price:String
+    var imageName:String
+}
