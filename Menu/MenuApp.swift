5dfcc05a402dad7b79daafbd387691dd66c6e2e4
Kal1219 <kaleb.cortes1208@gmail.com>
2023-03-30 13:40:38 -0600

    Initial Commit

---
+++ Menu/MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuApp.swift
+//  Menu
+//
+//  Created by Kaleb on 30/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
